ee264d20a1f876c2c2a3a49ed10ab31845f50743
Reynaldo Huanca Calderón <143009714+TarkusVI1@users.noreply.github.com>
2025-10-30 17:15:22 -0400

    Scripts Basicos
    Movimiento del personaje, Version inicial

---
+++ FruitManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FruitManager : MonoBehaviour
+{
+    public Text levelCleared;
+    private void Update()
+    {
+        AllFruitsCollected();
+    }
+    public void AllFruitsCollected()
+    {
+        if (transform.childCount == 0)
+        {
+            Debug.Log("No quedan frutas, Victoria");
+            levelCleared.gameObject.SetActive(true);
+            Invoke("ChangeScene", 1);
+        }
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+}
